ef815bbccf18f5669fdb6bc4064bbbb6ff12505d
René Godoy <52167950+Rego95@users.noreply.github.com>
2023-05-12 17:34:34 -0400

    primer commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+import { UserAuth } from "../context/AuthContext";
+
+export function Home() {
+  const { user, logOut } = UserAuth();
+
+  const cerrarSesion = async () => {
+    try {
+      await logOut();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Container>
+      <h1>Bienvenido {user.displayName}</h1>
+      <button onClick={cerrarSesion}>Cerrar Sesión</button>
+    </Container>
+  );
+}
+
+const Container = styled.div``;
